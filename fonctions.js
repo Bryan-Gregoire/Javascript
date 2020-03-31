2ca00be3a0950a5099347ac405eb03be48aee607
Gregoire <53735@etu.he2b.be>
2020-03-31 02:06:31 +0200

    étape 2 : css et html.
    Signed-off-by: Gregoire <53735@etu.he2b.be>

--- fonctions.js
+++ fonctions.js
@@ -1,5 +1,12 @@
-function displayDescription(){
-    for(let i = 0; i < data.length; i++){
+// étape 1
+function displayDescription() {
+    for (let i = 0; i < data.length; i++) {
         console.log(data[i].description);
     }
+}
+// fin étape 1
+
+// étape 2
+function addOption() {
+    
 }
