d4bd5f36cc8aba7f0a3465673d9c1c52d9faf30f
Gregoire <53735@etu.he2b.be>
2020-03-31 19:06:51 +0200

    étape 2 remplire formulaire
    Signed-off-by: Gregoire <53735@etu.he2b.be>

--- fonctions.js
+++ fonctions.js
@@ -2,16 +2,28 @@
 function displayDescription() {
     for (let i = 0; i < data.length; i++) {
         console.log(data[i].description);
+        console.log(data[i].id);
     }
 }
 // fin étape 1
 
 // étape 2
-$(document).ready(function() {
+$(document).ready(function () {
     for (let index = 0; index < data.length; index++) {
         let element = data[index].id;
         let descript = data[index].description;
-        let item = $("<option id=descript").text(descript);
-        $("langages").append(item)
-    };
+        let item = $('<option name value ></option>)');
+        $('#lang').append(item.text(descript));
+        $(item).attr("name", element);
+        $(item).attr("value", descript);
+    }
+});
+
+$(document).ready(function () {
+    $('#validate').click(function () {
+        let selectedValue = $('#lang').val();
+        let selectedName = $('#lang option:selected').text();
+        console.log(selectedValue);
+        console.log(selectedName);
+    });
 });
