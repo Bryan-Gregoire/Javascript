01e2bf90b784bc2b613e6a9cc93c8c21ae547ca7
Gregoire <53735@etu.he2b.be>
2020-04-01 23:43:18 +0200

    étape 6 terminée
    Signed-off-by: Gregoire <53735@etu.he2b.be>

--- Javascript/jeu.js
+++ Javascript/jeu.js
@@ -3,10 +3,11 @@ let nom = new URL(location.href).searchParams.get("quizId");
 console.log(nom);
 // étape 3 fin.
 
-// étape 4 et 5
+// étape 4, 5 et 6.
 
-// Afficher le thème du quizz choisis, la question le numéro de la question et inserer les boutons
-// avec les mots dedans dans l'ordre.
+// Afficher le thème du quizz choisis, la question le numéro de la question. 
+// Creer tableau avec les mots dedans dans l'ordre.
+// Mélanger les mots et inserer un boutons pour chaque mot.
 $(document).ready(function () {
     let j = 0;
     let answer;
@@ -23,10 +24,17 @@ $(document).ready(function () {
     }
     let texte = answer + " " + extras; // Contien la réponse et les extras.
     let mots = texte.split(' ');   // Tableau qui est composé de chaines de texte séparé par un espace. 
+    console.log(mots);
+    let counter = mots.length;
+    while(counter > 0) {
+        let index = Math.floor(Math.random()*counter);
+        counter--;
+        [mots[counter], mots[index]] = [mots[index], mots[counter]]; // mélanger les mots dans le tableau.
+    }
     for (let index = 0; index < mots.length; index++) {
-        let buts = $('<button></button');
+        let buts = $('<button id = bouton></button');
         $('#mots').append(buts.text(mots[index])); // Inserer les boutons avec les mots dedans, dans l'ordre.
     }
     console.log(mots);
 });
-// étape 4 et 5 fin.
+// étape 4, 5 et 6 fin.
