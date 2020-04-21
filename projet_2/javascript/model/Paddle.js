8426921570f7930168190f59545947712b5dbdd8
Gregoire Bryan <53735@etu.he2b.be>
2020-04-21 02:43:47 +0200

    Documentation

--- projet_2/javascript/model/Paddle.js
+++ projet_2/javascript/model/Paddle.js
@@ -1,13 +1,31 @@
+/**
+ * The paddle of the game.
+ * 
+ */
 class Paddle {
 
+    /**
+     * The constructor of paddle.
+     * 
+     * @param {*} left 
+     */
     constructor(left){
         this.__left = left;
     }
 
+    /**
+     * Get the left.
+     * 
+     */
     get left(){
         return this.__left;
     }
 
+    /**
+     * Set the left.
+     * 
+     * @param {*} left the left to set.
+     */
     moveTo(left){
         this.__left = left;
     }
--- projet_2/javascript/view/View.js
+++ projet_2/javascript/view/View.js
@@ -1,9 +1,22 @@
+/**
+ * View of the paddle.
+ */
 class View {
     
+    /**
+     * Display the paddle.
+     * 
+     * @param {*} paddle the paddle.
+     */
     displayPaddle(paddle){
         $('#raquette').css('left', paddle.left);
     }
 
+    /**
+     * return the distance of any element of id unId compared 
+     * at the left edge of the window.
+     * 
+     */
     sceneLeft(){
         return $("#scene").offset().left;
     }
