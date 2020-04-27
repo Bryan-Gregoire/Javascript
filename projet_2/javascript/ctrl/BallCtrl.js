f5d392765f2522c1f819e1210537cb77b32836a7
Gregoire Bryan <53735@etu.he2b.be>
2020-04-27 22:53:25 +0200

    Étape 2.4: Une balle qui bouge toute seule

---
+++ projet_2/javascript/ctrl/BallCtrl.js
@@ -0,0 +1,39 @@
+/** 
+* Contrôleur de la balle.
+* Propose des méthodes pour démarrer / arrêter la balle et la faire bouger.
+*/
+class BallCtrl {
+
+    /**
+   * @param { Ball } ball - la balle à contrôler
+   * @param { View } view - la vue
+   */
+    constructor(ball, view) {
+        this._ball = ball;
+        this._view = view;
+        view.displayBall(ball);
+    }
+
+    /**
+     * Démarre la balle.
+     * 
+     */
+    start() {
+        this._moveListener = setInterval(() => this.move(), 10);
+    }
+
+    /**
+     * Déplace la balle d’un pas (défini par son mouvement) et rafraichit la vue.
+     */
+    move() {
+        this._ball.move();
+        this._view.displayBall(this._ball);
+    }
+
+    /**
+     * Arrête la balle.
+     */
+    stop() {
+        clearInterval(this._moveListener);
+    }
+}
