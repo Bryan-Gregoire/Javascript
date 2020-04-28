0ca1b08894e4a201183daabe68ca64ae2e6c05c1
Gregoire Bryan <53735@etu.he2b.be>
2020-04-28 21:52:12 +0200

    Étape 2.6: Une façade pour le modèle et une pour le contrôleur

--- projet_2/javascript/ctrl/GameCtrl.js
+++ projet_2/javascript/ctrl/GameCtrl.js
@@ -3,8 +3,8 @@ class GameCtrl {
     constructor(game, view) {
         this.__game = game;
         this.__view = view;
-        this.__paddleCtrl = PaddleCtrl;
-        this.__ballCtrl = BallCtrl;
+        this.__paddleCtrl = new PaddleCtrl(this.__game, this.__view);
+        this.__ballCtrl = new BallCtrl(this.__game, this.__view);
     }
 
     play() {
