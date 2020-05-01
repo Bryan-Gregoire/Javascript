bfd4ab8ab28ea4f043f7a6017c5ef1545a34fd5f
Gregoire Bryan <53735@etu.he2b.be>
2020-05-01 17:22:05 +0200

    Changement de la vitesse

--- projet_2/javascript/model/Game.js
+++ projet_2/javascript/model/Game.js
@@ -9,7 +9,7 @@ class Game {
      */
     constructor() {
         this.__paddle = new Paddle(sceneWidth / 2 - paddleWidth / 2);
-        this.__ball = new Ball(new Position(this.randomPosX(), sceneHeight / 2 - ballHeight / 2), new Movement(this.randomDeltaX(), -3));
+        this.__ball = new Ball(new Position(this.randomPosX(), sceneHeight / 2 - ballHeight / 2), new Movement(this.randomDeltaX(), -1));
     }
 
     /**
